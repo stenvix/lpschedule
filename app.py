6f9488d463ef8b02a4fdc2cdbfd384d9dbacaf53
Stepanov Valentin <falken.ua@gmail.com>
2016-06-02 23:47:37 +0300

    Add travis ci test configuration

--- app.py
+++ app.py
@@ -10,37 +10,32 @@ import os
 import sys
 
 try:
-    virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-    python_version = "python"+str(sys.version_info[0])+"."+str(sys.version_info[1])
-    os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib', python_version, 'site-packages')
-    virtualenv = os.path.join(virtenv, 'bin','activate_this.py')
-    if(sys.version_info[0] < 3):
-        execfile(virtualenv, dict(__file__=virtualenv))
-    else:
-        exec(open(virtualenv).read(), dict(__file__=virtualenv))
-
+  virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+  python_version = "python"+str(sys.version_info[0])+"."+str(sys.version_info[1]) 
+  os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib', python_version, 'site-packages')
+  virtualenv = os.path.join(virtenv, 'bin','activate_this.py')
+  if(sys.version_info[0] < 3):
+    execfile(virtualenv, dict(__file__=virtualenv))
+  else:
+    exec(open(virtualenv).read(), dict(__file__=virtualenv))
+    
 except IOError:
-      pass
+  pass
 
-  #
-  # IMPORTANT: Put any additional includes below this line.  If placed above
-  # this
-  # line, it's possible required libraries won't be in your searchable path
-  #
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
 
 
-  #
-  #  main():
-  #
+#
+#  main():
+#
 if __name__=='__main__':
     application = imp.load_source('app', 'manage.py')
     port = application.app.config['PORT']
     ip = application.app.config['IP']
-    app_name = application.app.config['APP_NAME']
-    host_name = application.app.config['HOST_NAME']
-
     try:
-        application.gunicorn(host_name,port,workers=1)
+        application.gunicorn(ip,port,workers=1)
     except ImportError:
         pass
-
---
+++ tests/server/test_simple.py
@@ -0,0 +1,6 @@
+def simple_test():
+    return 1
+
+
+def test_simple_test():
+    assert simple_test() == 1
