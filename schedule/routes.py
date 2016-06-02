dc9d738152d2b67eb7e81987e32e0ee37cf08d63
Stepanov Valentin <falken.ua@gmail.com>
2016-06-03 00:36:15 +0300

    Fix route mistake, add setup file

--- schedule/routes.py
+++ schedule/routes.py
@@ -3,4 +3,3 @@ from flask import render_template
 @app.route('/')
 def index():
 	return render_template('index.html')
-@app.route('/start':w
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='LpSchedule',
+      version='0.1',
+      description='API for Lviv Polytechnik schedule',
+      author='Stepanov Valentyn',
+      author_email='mr.valentyn.stepanov@gmail.com',
+      url='http://example.com',
+      install_requires=[
+		'Flask==0.10.1',
+		'Flask-Script>=2.0.5',
+		'grab>=0.6.30',
+		'gunicorn>=19.4.5',
+		'itsdangerous>=0.24',
+		'Jinja2>=2.6',
+		'lxml>=3.5.0',
+		'MarkupSafe>=0.11',
+		'pycurl>=7.43.0',
+		'pytils>=0.3',
+		'selection>=0.0.11',
+		'six>=1.7.3',
+		'user-agent>=0.1.1',
+		'weblib>=0.1.20',
+		'Werkzeug>=0.8.3'],
+)
