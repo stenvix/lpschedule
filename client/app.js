8c20ae78046b65c40cda6e1eb193464ffb2cefe5
Stepanov Valentin <falken.ua@gmail.com>
2016-06-09 12:52:42 +0300

    Add webpack support, add material design components, start scraper for lp.edu.ua page

---
+++ client/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by stepanov on 6/8/16.
+ */
+
+
+
+require('./sass/main.sass');
+
+console.log('This is work');
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = [{
+  context: __dirname+ '/client',
+  entry: {
+    bundle: "./app.js"
+  },
+  output: {
+    path: __dirname + '/schedule/static',
+    filename: "js/[name].js"
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+    }, {
+      test: /\.sass$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+    }],
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[name].css'),
+  ]
+}];
