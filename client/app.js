96af5b68273293727541ffdc3edbb22ac8cad9cb
Stepanov Valentin <falken.ua@gmail.com>
2016-07-20 21:49:52 +0300

    Add mobile swipe gestures, change week message

--- client/app.js
+++ client/app.js
@@ -5,3 +5,41 @@
 require('./sass/main.sass');
 require('material-design-lite/material.min.css');
 require('material-design-lite/material.min.js');
+
+require('jquery.event.move');
+require('jquery.event.swipe');
+
+
+$(document).ready(function () {
+    var snackbarContainer = document.querySelector('#messages');
+    $('#timetable')
+        .on('movestart', function (e) {
+            // If the movestart is heading off in an upwards or downwards
+            // direction, prevent it so that the browser scrolls normally.
+            if ((e.distX > e.distY && e.distX < -e.distY) ||
+                (e.distX < e.distY && e.distX > -e.distY)) {
+                e.preventDefault();
+            }
+        });
+    $('#timetable').on('swipeleft', function (event) {
+        $('.mdl-tabs__tab').each(function (key, value) {
+            if ($(value).hasClass('is-active') && key == 0) {
+                $('.mdl-tabs__tab').toggleClass('is-active');
+                $('.mdl-tabs__panel').toggleClass('is-active');
+            }else if(key==1){
+                snackbarContainer.MaterialSnackbar.showSnackbar({message:$(value).text()});
+            }
+        });
+    });
+    $('#timetable').on('swiperight', function (event) {
+        $('.mdl-tabs__tab').each(function (key, value) {
+            if ($(value).hasClass('is-active') && key == 1) {
+                $('.mdl-tabs__tab').toggleClass('is-active');
+                $('.mdl-tabs__panel').toggleClass('is-active');
+            }else if(key==0){
+                snackbarContainer.MaterialSnackbar.showSnackbar({message:$(value).text()});
+            }
+        });
+
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = [{
-  context: __dirname+ "/client",
-  entry: {
-    bundle: "./app.js"
-  },
-  output: {
-    path: __dirname + "/schedule/static",
-    filename: "js/[name].js"
-  },
-  module: {
-    loaders: [{
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-    }, {
-      test: /\.sass$/,
-      loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-    }],
-  },
-  plugins: [
-    new ExtractTextPlugin("css/[name].css"),
-  ]
+    context: __dirname + "/client",
+    entry: {
+        bundle: "./app.js"
+    },
+    output: {
+        path: __dirname + "/schedule/static",
+        filename: "js/[name].js"
+    },
+    externals: {
+        // require("jquery") is external and available
+        //  on the global var jQuery
+        "jquery": "jQuery"
+    }
+    ,
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        }, {
+            test: /\.sass$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+        }],
+    },
+    plugins: [
+        new ExtractTextPlugin("css/[name].css"),
+    ]
 }];
