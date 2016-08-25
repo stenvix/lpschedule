9ba6afffee21acf463cb455c8a1a082ef09715b3
Stepanov Valentin <falken.ua@gmail.com>
2016-08-25 22:44:07 +0300

    Openshift startup fix, change vagga run command name
    Rename model file.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """Flask Script configuration."""
+import os
+import sys
+
 from werkzeug.wsgi import DispatcherMiddleware
 from schedule import frontend, api
 
@@ -22,8 +25,30 @@ def gunicorn(app, host, port, workers):
     application = FlaskApplication(app)
     return application.run()
 
+
+def server(app):
+    try:
+        virtenv = os.path. \
+            join(os.environ.get('OPENSHIFT_PYTHON_DIR', '.'), 'virtenv')
+        python_version = 'python' + str(sys.version_info[0]) + \
+                         '.' + str(sys.version_info[1])
+        os.environ['PYTHON_EGG_CACHE'] = os.path.join(
+            virtenv, 'lib', python_version, 'site-packages')
+        virtualenv = os.path.join(virtenv, 'bin', 'activate_this.py')
+        if (sys.version_info[0] < 3):
+            execfile(virtualenv, dict(__file__=virtualenv))
+        else:
+            exec(open(virtualenv).read(), dict(__file__=virtualenv))
+    except IOError:
+        pass
+
+    port = app.app.config['PORT']
+    server_ip = app.app.config['IP']
+    if port is not None and server is not None:
+        gunicorn(app, server_ip, port, workers=1)
+
 if __name__ == '__main__':
     app = DispatcherMiddleware(frontend.create_app(), {
         '/api': api.create_app()
     })
-    gunicorn(app, '0.0.0.0', '5000', 2)
+    server(app)
