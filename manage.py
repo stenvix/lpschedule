875898ce58ec3f7616b090c0134554df3120be3d
Stepanov Valentin <falken.ua@gmail.com>
2016-08-28 10:34:33 +0300

    Setup development enviroment

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 from flask_script import Manager
-from schedule.api import create_app
+from schedule import frontend, api
+from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.serving import run_simple
 
 
-manager = Manager(create_app())
+def runserver():
+    app = DispatcherMiddleware(frontend.create_app(), {'/api': api.create_app()})
+    run_simple('0.0.0.0', 5000, app, use_reloader=True, use_debugger=True)
+
+manager = Manager(frontend.create_app())
+manager.add_command('runserver', runserver())
+
 
 if __name__ == '__main__':
-    manager.run()
+    manager.run(default_command='runserver')
